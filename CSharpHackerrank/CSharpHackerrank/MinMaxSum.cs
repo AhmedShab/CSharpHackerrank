a5a559170bd298bf3bbff357ecfdad3ddb069986
AhmedShab <ahmed.vuw@gmail.com>
2017-07-01 12:11:54 +1200

    Done with the min max challenge

---
+++ CSharpHackerrank/CSharpHackerrank/MinMaxSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHackerrank
+{
+	public class MinMaxSum
+	{
+		public class MiniMaxSum
+		{
+			public string SolveMinMax()
+			{
+				IList<long> results = new List<long>();
+				// string[] arr_temp = Console.ReadLine().Split(' ');
+				int[] arr = { 1, 2, 3, 4, 5 };
+
+				foreach (var number in arr)
+				{
+					results.Add(number);
+				}
+
+				long sum = results.Sum(n => n);
+
+				long min = sum - results.Max(n => n);
+				long max = sum - results.Min(n => n);
+
+
+				return ($"{min} {max}");
+			}
+		}
+	}
+}
---
+++ CSharpHackerrank/CSharpHackerrank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHackerrank
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MinMaxSum.MiniMaxSum miniMax = new MinMaxSum.MiniMaxSum();
+
+			Console.WriteLine(miniMax.SolveMinMax());
+		}
+	}
+}
